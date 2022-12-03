8531f7f9ed9271e4714e828d6b10ce57026ea18a
Fernando Ritter <fernandolah@gmail.com>
2022-12-03 18:15:05 -0300

    moving

---
+++ source/calc/indicators/movingaveragecalc.cpp
@@ -0,0 +1,6 @@
+#include "movingaveragecalc.h"
+
+MovingAverageCalc::MovingAverageCalc()
+{
+
+}
---
+++ source/calc/indicators/movingaveragecalc.h
@@ -0,0 +1,12 @@
+#ifndef MOVINGAVERAGECALC_H
+#define MOVINGAVERAGECALC_H
+
+#include "customarrayindicatorcalc.h"
+
+class MovingAverageCalc : public CustomArrayIndicatorCalc
+{
+public:
+    MovingAverageCalc();
+};
+
+#endif // MOVINGAVERAGECALC_H
